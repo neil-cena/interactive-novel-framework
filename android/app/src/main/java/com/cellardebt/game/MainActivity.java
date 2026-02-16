5abf9ac85059a5dbda98d6b83b4cba3dd0b58e4b
Neil Allen Cena <neilcena.dev@gmail.com>
2026-02-16 12:44:51 +0800

    feat(mobile): configure Capacitor 8

---
+++ android/app/src/main/java/com/cellardebt/game/MainActivity.java
@@ -0,0 +1,5 @@
+package com.cellardebt.game;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli'
+
+const config: CapacitorConfig = {
+  appId: 'com.cellardebt.game',
+  appName: 'The Cellar Debt',
+  webDir: 'dist',
+  bundledWebRuntime: false,
+}
+
+export default config
